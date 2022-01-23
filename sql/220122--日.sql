f7a8b59ec751af0e63de37ad5c382fbf45aa9d2c
Liu <liuguojunjp@gmail.com>
2022-01-24 07:07:38 +0900

    2201240707

--- sql/220122--日.sql
+++ sql/220122--日.sql
@@ -1,3 +1,7 @@
 select * from users where id =1;
 
 
+select `posts`.*, `users`.`name`, `users`.`email`
+from `posts`
+         inner join `users` on `users`.`id` = `posts`.`user_id` and `users`.`email_verified_at` is not null
+where `posts`.`views` > 0;
